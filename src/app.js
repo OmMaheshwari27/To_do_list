a4e85ce11a0c7f2339a2fe89927736934cd083b3
OmMaheshwari27 <oom12345609@gmail.com>
2025-07-17 18:41:43 +0530

    removed extra code

--- src/app.js
+++ src/app.js
@@ -25,5 +25,5 @@ app.use("/", TaskRouter);
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`✅ Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
