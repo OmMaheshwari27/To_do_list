8166acbf3c0ff9f413be274ea3e416502ef20c06
OmMaheshwari27 <oom12345609@gmail.com>
2025-07-17 19:50:41 +0530

    Fixed issue

--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,30 @@
 const mongoose = require("mongoose");
+const validator=require("validator");
 
 const userSchema = new mongoose.Schema({
 
     firstName: {
         type: String,
         required: true,
+        trim:true,
     },
     lastName:{
         type:String,
+        trim: true,
+
     },
     //email is set as unique so that one user can register using one email
     email: {
         type: String,
         required: true,
         unique:true,
+        trim:true,
+        lowercase:true,
+        validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid email");
+      }
+    }
     },
     password:{
         type: String,
