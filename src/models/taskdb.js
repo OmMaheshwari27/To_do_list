303caeab3b2ff20e14d14a936c1dec61bf9e432f
OmMaheshwari27 <oom12345609@gmail.com>
2025-07-17 19:25:21 +0530

    Added user field to link taks

--- src/models/taskdb.js
+++ src/models/taskdb.js
@@ -2,32 +2,37 @@ const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
 
-    title: {
-        type: String,
-        required: true,
-        ref:"user",
-        unique: true,
-    },
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
 
-    description: {
-        type: String,
-        required: true,
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    // only three status are allowed, if other then error will be displayed
+    enum: {
+      values: ["pending", "in-progress", "complete"],
+      message: `{VALUE} is not a valid status`,
     },
-    status: {
-        type: String,
-        // only three status are allowed, if other then error will be displayed
-        enum: {
-            values: ["pending", "in-progress", "complete"],
-            message: `{VALUE} is not a valid status`,
-        },
-        //default status is pending
-        default:"pending",
-    }
+    //default status is pending
+    default: "pending",
+  },
+  // storing user details to match the task of each user
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Task_Users",
+    required: true
+  }
 },
-    { timestamps: true }
+  { timestamps: true }
 ).pre("save", async function (next) {
   const Task = mongoose.model("Taskdb");
-  
+
   // Only check for duplicates when creating a new task
   if (this.isNew || this.isModified("title")) {
     const existing = await Task.findOne({ title: this.title });
